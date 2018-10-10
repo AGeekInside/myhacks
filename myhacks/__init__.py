23571501eca571a8fc38ff294e7c1bcb4466e054
Marc Brooks <mbrooks@mitre.org>
2018-10-10 08:56:23 -0400

    Commit adds something ot main level

--- myhacks/__init__.py
+++ myhacks/__init__.py
@@ -2,6 +2,7 @@ __version__ = "15.0.0"
 
 from .hacks import compile_repo_info
 from .hacks import find_repos
+from .hacks import get_hostname_ip
 from .hacks import make_executable
 from .hacks import HacksInfo
 
@@ -12,4 +13,10 @@ PROJS_DIR = "/home/mbrooks/projs/"
 OUTPUTS = ["fancy_grid", "grid", "psql", "presto", "simple"]
 
 
-__all__ = ["compile_repo_info", "find_repos", "HacksInfo", "make_executable"]
+__all__ = [
+    "compile_repo_info",
+    "find_repos",
+    "get_hostname_ip",
+    "HacksInfo",
+    "make_executable",
+]
